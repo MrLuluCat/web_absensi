48db47b2103c7774354bb94094b1e6e47c9fe6d8
thenabil11@gmail.com <126738815+MrLuluCat@users.noreply.github.com>
2023-05-11 07:22:49 +0700

    Update index.blade.php

--- resources/views/presensiCalas/index.blade.php
+++ resources/views/presensiCalas/index.blade.php
@@ -67,8 +67,7 @@
                                 <div class="modal-content">
                                     <div class="modal-header">
                                         <h1 class="modal-title fs-5" id="exampleModalLabel">Hapus Data?</h1>
-                                        <button type
-="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                           </div>
                           <div class="modal-body"> 
                             <p>Apakah Anda Yakin Untuk Menghapus Entry Data Ini?</p>
